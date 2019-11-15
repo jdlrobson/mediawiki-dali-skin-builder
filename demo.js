bb9bf59b4067e746dd368fae0706aaaeaf55dd4d
jdlrobson <jdlrobson@gmail.com>
2019-11-15 08:42:58 -0800

    Cache API response for the day
    Change-Id: Ifd94599c865dfa05daa3c8d44c124771e92ac6a2

--- demo.js
+++ demo.js
@@ -8,12 +8,22 @@ const HOST = 'https://en.wikipedia.org';
 const TITLE = 'Salvador_Dalí';
 
 const template = fs.readFileSync(__dirname + '/templates/skin.mustache', 'utf8' );
+const KEY = (new Date()).toISOString().substr(0,10);
 
-fetch( `${HOST}/w/api.php?action=parse&format=json&page=${encodeURIComponent(TITLE)}&prop=text%7Clanglinks%7Ccategorieshtml%7Ccategories%7Clinks%7Ctemplates%7Cimages%7Cexternallinks%7Csections%7Crevid%7Cdisplaytitle%7Ciwlinks%7Cproperties%7Cparsewarnings&formatversion=2&origin=*` ).then( function ( resp ) {
-    return resp.json();
-} ).then( function ( data ) {
+let dataPromise;
+if ( localStorage.getItem(KEY) ) {
+    dataPromise = Promise.resolve(JSON.parse(localStorage.getItem(KEY)));
+} else {
+    dataPromise = fetch( `${HOST}/w/api.php?action=parse&format=json&page=${encodeURIComponent(TITLE)}&prop=text%7Clanglinks%7Ccategorieshtml%7Ccategories%7Clinks%7Ctemplates%7Cimages%7Cexternallinks%7Csections%7Crevid%7Cdisplaytitle%7Ciwlinks%7Cproperties%7Cparsewarnings&formatversion=2&origin=*` ).then( function ( resp ) {
+        return resp.json();
+    } ).then( (json) => {
+        localStorage.setItem(KEY, JSON.stringify(json));
+        return json;
+    })
+}
+
+dataPromise.then( function ( data ) {
     const parse = data.parse;
-    console.log(parse.text);
     document.body.innerHTML = mustache.render( template,
         Object.assign( {}, demoData, {
             'html-pagetitle': parse.displaytitle,
